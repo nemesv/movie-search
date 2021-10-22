33b2672225a5544b8096bb0fdf08be9c23c9ac6b
Viktor Nemes <nemesv@gmail.com>
2021-10-22 08:08:29 +0200

    Add basic styling for the SearchResult component

--- src/components/MovieSearchResult.js
+++ src/components/MovieSearchResult.js
@@ -6,20 +6,23 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Link, useRouteMatch } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
 
 export function MovieSearchResult({ rows, limit = 10 }) {
   let { url } = useRouteMatch();
   return (
-    <div style={{ maxWidth: 650 }}>
-      <h1>Results:</h1>
+    <Container>
+      <Typography variant="h7">Results:</Typography>
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>Title</TableCell>
-              <TableCell align="right">Year</TableCell>
-              <TableCell align="right">Genre</TableCell>
-              <TableCell align="right">Score</TableCell>
+              <TableCell>Year</TableCell>
+              <TableCell>Genre</TableCell>
+              <TableCell>Score</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -38,6 +41,6 @@ export function MovieSearchResult({ rows, limit = 10 }) {
           </TableBody>
         </Table>
       </TableContainer>
-    </div>
+    </Container>
   );
 }
