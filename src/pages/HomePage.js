ee35c15589bb455deb562c850f4c7d88ec91ebeb
Viktor Nemes <nemesv@gmail.com>
2021-10-22 08:11:22 +0200

    Add basic styling for the HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,13 @@
 import { SearchForm } from "../components/SearchForm";
 import { useHistory } from "react-router-dom";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-
 
 export function HomePage() {
   const history = useHistory();
   return (
     <Container>
-      <h1>Welcome to Movie Search!</h1>
+      <Typography variant="h3">Welcome to Movie Search!</Typography>
       <SearchForm
         label="Movie title"
         onSearch={(value) => history.push(`/search/${value}`)}
